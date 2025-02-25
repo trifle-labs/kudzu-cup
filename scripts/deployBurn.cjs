2b844198cc63554e20896b2733d1a1cffc823cb5
Billy Rennekamp <billy.rennekamp@gmail.com>
2025-02-25 11:08:06 +0100

    run deploy locally

--- scripts/deployBurn.cjs
+++ scripts/deployBurn.cjs
@@ -1,16 +1,17 @@
-const hre = require("hardhat");
+const hre = require('hardhat');
 
 async function main() {
   const [deployer] = await hre.ethers.getSigners();
   console.log({ deployer: deployer.address });
-  const networkinfo = await hre.network.provider.send("eth_chainId");
+  const networkinfo = await hre.network.provider.send('eth_chainId');
   const chainId = BigInt(networkinfo);
-  console.log("Deploy to chain:");
+  console.log('Deploy to chain:');
   console.log({ chainId });
-  const { initContracts, deployBurn } = await import("./utils.js");
+  const { initContracts, deployBurn } = await import('./utils.js');
   const { Kudzu } = await initContracts();
   await deployBurn({
-    Kudzu: { target: "0x822eca148785eca2c465053553c06cb4c52c5f7c" },
+    // Kudzu: { target: "0x822eca148785eca2c465053553c06cb4c52c5f7c" },
+    Kudzu,
     ignoreTesting: true,
     saveAndVerify: true,
   });
