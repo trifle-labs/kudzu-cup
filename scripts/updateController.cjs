4c7271b8c93bd1d2715d69db1dfab53ac85c7bff
Billy Rennekamp <billy.rennekamp@gmail.com>
2025-03-14 17:30:27 +0100

    re-re-re-redeploy

--- scripts/updateController.cjs
+++ scripts/updateController.cjs
@@ -16,11 +16,11 @@ async function main() {
     console.log(`KudzuBurn is paused`);
   }
 
-  const allPrevControllers = [
-    '0xE30cef8e99A6eEbE3CBF2862641337f57830FbeE',
-    '0x23dEf29FAF850A7C5F18C610c34945e65ff334C0',
-    '0xA62a48D61bF012F70c8C865Dc17e59F6E6761106',
-  ];
+  // const allPrevControllers = [
+  //   '0xE30cef8e99A6eEbE3CBF2862641337f57830FbeE',
+  //   '0x23dEf29FAF850A7C5F18C610c34945e65ff334C0',
+  //   '0xA62a48D61bF012F70c8C865Dc17e59F6E6761106',
+  // ];
 
   const KudzuBurnController = await hre.ethers.getContractFactory(
     'KudzuBurnController'
@@ -38,13 +38,13 @@ async function main() {
     KudzuBurnController: burnController,
   };
 
-  for (let i = 0; i < allPrevControllers.length; i++) {
-    const tx = await burnController.addPrevController(allPrevControllers[i]);
-    await tx.wait();
-    console.log(
-      `PrevController ${allPrevControllers[i]} added to KudzuBurnController ${burnController.target}`
-    );
-  }
+  // for (let i = 0; i < allPrevControllers.length; i++) {
+  //   const tx = await burnController.addPrevController(allPrevControllers[i]);
+  //   await tx.wait();
+  //   console.log(
+  //     `PrevController ${allPrevControllers[i]} added to KudzuBurnController ${burnController.target}`
+  //   );
+  // }
 
   // update ExternalMetadata
   const tx = await KudzuBurn.updateKudzuBurnController(burnController.target);
